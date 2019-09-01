08315a40810258d59adbe312d6a16cf007511927
nick hatcher <noliviah@outlook.com>
2019-08-31 22:22:39 -0400

    algorithm working. need to compute strongly connected components

--- graph/main.cpp
+++ graph/main.cpp
@@ -14,21 +14,19 @@ using namespace std;
 
 struct Vertex {
     bool explored;
-    int finishingTime;
     int leader;
-    int SSC;//strongly connected compentent ID
 };
 
 int t = 0; //global variable to track finishing times (e.g. - number of nodes processed)
 int s = NULL;//global veraible to track the source vertex from which DFS was initaited
 vector<Vertex> vertices(10);
+vector<int> finishing(10);
 
-void DFS_Loop(vector<vector<int>>&);
-void DFS(vector<vector<int>>&, size_t, bool);
+void DFS_Loop(vector<vector<int>>&, int);
+void DFS(vector<vector<int>>&, size_t);
 vector<vector<int>> createGraph(fstream&);
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
     
     fstream f {"list.txt", ios::in | ios::binary}; //open file for input
     
@@ -36,59 +34,56 @@ int main(int argc, const char * argv[]) {
         cout << "File could not be opened\n";
         return 9;
     }
-    
-    vector<vector<int>> g = createGraph(f);
-    
-    for (int i = 0; i < 10; ++i){
-        vertices[i].explored = false;
-    }
-    
-    DFS_Loop(g);
-    
-    for (int i = 0; i < 10; ++i){
-        cout << "i: " << i << " finishing: " << vertices[i].finishingTime << endl;
-    }
-    return 0;
-}
 
-vector<vector<int>> createGraph(fstream& f){
     int numVertices = 10;
     int head;
     int tail;
     
     vector<vector<int>> g(numVertices);
-    
-    while (f >> tail >> head) {//read line from file
-//        g[tail].push_back(head);//create edge
-        g[head].push_back(tail);//create edge
+    vector<vector<int>> g_rev(numVertices);
 
+    while (f >> tail >> head) {//read line from file
+        g[tail].push_back(head);//create edge
+        g_rev[head].push_back(tail);//create edge
     }
-    return g;
+    
+    for (int i = 0; i < numVertices; ++i){
+        vertices[i].explored = false;
+    }
+    
+    DFS_Loop(g_rev, 1);
+    
+    for (int i = 0; i < numVertices; ++i){
+        vertices[i].explored = false;
+    }
+    
+    DFS_Loop(g, 2);
+
+    return 0;
 }
 
-void DFS_Loop(vector<vector<int>>& g) {
+void DFS_Loop(vector<vector<int>>& g, int pass) {
     for(size_t i = g.size() - 1; i >= 1; --i){
-        if (!vertices[i].explored){
-            s = i;
-            DFS(g, i, 1);
+        int node = (pass == 1 ? i : finishing[i]);
+
+        if (vertices[node].explored){
+            s = node;
+            DFS(g, node);
         }
     }
 }
 
-void DFS(vector<vector<int>>& g, size_t i, bool reverse=false){
+void DFS(vector<vector<int>>& g, size_t i){
     vertices[i].explored = true;
     vertices[i].leader = s;
     for (int j = 0; j < g[i].size(); ++j){
         int v = g[i][j];
-        if (v == 0 ){
-            cout << "ddd ";
-        }//cout << "i: " << i << endl;
-        //cout << "j: " << j << endl;
-        //cout << "v: " << g[i][j] << endl;
+
         if (!vertices[v].explored){
-            DFS(g, j);
+            DFS(g, v);
         }
     }
     ++t;
-    vertices[i].finishingTime = t;
+    finishing[t] = i;//capture finishing time of each node
+    //vertices[i].finishingTime = t;
 }
